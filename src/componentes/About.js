3496352000356839b477412931b8e05d4ff1dd99
Ismael Chaves <ismaelchaves2001@outlook.com>
2023-08-25 11:34:11 -0300

    Atualização 25.08.2023

--- src/componentes/About.js
+++ src/componentes/About.js
@@ -44,22 +44,43 @@ function About() {
       <footer>
         <div>
           <p>
-            <img src={Motor} alt='Motor' />
+            <img src={Motor} alt='Motor' 
+            title='
+            Tipo: OHC, monocilíndrico, 4 tempos, arrefecido a ar.
+            Cilindrada: 293,5 cc
+            Potência Máxima: 24,5 CV a 7.500 rpm (gasolina) / 24,7 CV a 7.500 rpm (etanol)
+            Torque Máximo: 2,61 kgf.m a 5.500 rpm (gasolina) / 2,67 kgf.m a 5.500 (etanol)
+            Transmissão: 6 velocidades
+            Sistema de Partida: Elétrica
+            Diâmetro x Curso: 77,0 x 63,0 mm
+            Relação de Compressão: 9,3 : 1
+            Sistema Alimentação: Injeção Eletrônica, PGM FI
+            Combustível: Gasolina / Álcool'
+            />
           </p>
         </div>
         <div>
           <p>
-            <img src={GPS} alt='Painel GPS' />
+            <img src={GPS} alt='Painel GPS' 
+            title='
+            Tecnológica, a CB 300F Twister conta agora com um painel blackout surpreendente, 100% digital e com indicador de marcha inspirado nas motos de alta cilindrada, que facilita a leitura das informações independentemente da luminosidade.'/>
           </p>
         </div>
         <div>
           <p>
-            <img src={Roda} alt='Pneus' />
+            <img src={Roda} alt='Pneus' 
+            title='
+            Estilo e performance: com rodas de liga leve de 17” totalmente repaginadas e pneu traseiro mais robusto, a nova CB 300F Twister proporciona maior aderência ao solo e um ótimo desempenho na pilotagem.
+            
+            Com aumento de 37 para 41 no diâmetro do garfo dianteiro e suspensão traseira ampliada de 108 mm para 120 mm, a CB 300F Twister possui ainda mais força para absorver impactos, garantindo controle e conforto mesmo nas vias irregulares.'/>
           </p>
         </div>
         <div>
           <p>
-            <img src={Tanque} alt='Tanque de Gasolina' />
+            <img src={Tanque} alt='Tanque de Gasolina'
+            title='
+            Tanque de Combustível: 14,1 litros
+            Óleo do Motor: 2 litros' />
           </p>
         </div>
       </footer>
--- src/componentes/Sidebar.js
+++ src/componentes/Sidebar.js
@@ -3,9 +3,19 @@ import '../App.css'
 import Shop from './imgs/shopping.png'
 
 function Sidebar(){
+
+    function toggleInformation() {
+        var info = document.getElementById('information');
+        info.style.display = info.style.display === "block" ? "none" : "block";
+      }
+    
     return(
         <div className="Sidebar">
-            <img src={Shop} alt="Carrinho de Compras"/>
+            <img src={Shop} alt="Carrinho de Compras" onClick={toggleInformation}/>
+
+            <div id='information'>
+                <p>O carrinho está vazio.</p>
+            </div>
         </div>
     )
 }
