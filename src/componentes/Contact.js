05e6aa6b0fa56de5d3e43105d632f26d22145956
Ismael Chaves <ismaelchaves2001@outlook.com>
2023-08-25 10:35:02 -0300

    Atualização de Otimização

--- src/componentes/Contact.js
+++ src/componentes/Contact.js
@@ -9,13 +9,13 @@ function Contact() {
                 <div className='Banner'>
                     <img src={Banner} alt='Imagem de Fundo' />
                 </div>
-                <div className='Formulario'>
+                <form className='Formulario'>
                     <h2>Contato</h2>
-                    <input type='text' placeholder='Insira seu Nome Completo' maxLength={45}/>
-                    <input type='email' placeholder='E-mail para Contato' maxLength={45}/>
-                    <textarea placeholder='Digite sua Mensagem'/>
-                    <button>Enviar</button>
-                </div>
+                    <input type='text' placeholder='Insira seu Nome Completo' required maxLength={45}/>
+                    <input type='email' placeholder='E-mail para Contato' required maxLength={45}/>
+                    <textarea placeholder='Digite sua Mensagem' required/>
+                    <input type='submit' value='Enviar' />
+                </form>
             </div>
         </div>
     )
